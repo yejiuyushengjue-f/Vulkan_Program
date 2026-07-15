c5073ce230cd5e3c05e1a665d0db19e76057e9b0
yejiuyushengjue-f <3529519863@qq.com>
2026-07-15 13:44:51 +0800

    创建初始窗口

--- run.sh
+++ run.sh
@@ -6,10 +6,15 @@ export PATH="/c/Program Files/CMake/bin:/ucrt64/bin:/usr/bin:${PATH}"
 
 project_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 build_dir="${project_dir}/build"
+cmake_exe="/c/Program Files/CMake/bin/cmake.exe"
 
-if [[ ! -f "${build_dir}/CMakeCache.txt" ]]; then
-    cmake -S "${project_dir}" -B "${build_dir}" -G Ninja
-fi
+"${cmake_exe}" \
+    -S "${project_dir}" \
+    -B "${build_dir}" \
+    -G Ninja \
+    -DCMAKE_MAKE_PROGRAM=C:/msys64/ucrt64/bin/ninja.exe \
+    -DCMAKE_C_COMPILER=C:/msys64/ucrt64/bin/gcc.exe \
+    -DCMAKE_CXX_COMPILER=C:/msys64/ucrt64/bin/g++.exe
 
-cmake --build "${build_dir}"
+"${cmake_exe}" --build "${build_dir}"
 "${build_dir}/vulkan.exe"
--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,62 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
+#include <cstdlib>
 #include <iostream>
+#include <stdexcept>
 
-int main() {
-    glfwInit();
+const int WIDTH = 800;
+const int HEIGHT = 600;
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::cout << extensionCount << " extensions supported\n";
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+class HelloTriangleApplication {
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
     }
 
-    glfwDestroyWindow(window);
+private:
+    GLFWwindow* window = nullptr;
 
-    glfwTerminate();
+    void initWindow() {
+        glfwInit();
 
-    return 0;
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
+    void initVulkan() {
+        // Vulkan initialization code goes here
+    }
+
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+        // Main rendering loop code goes here
+    }
+
+    void cleanup() {
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+        // Cleanup code goes here
+    }
+};
+
+int main() {
+    HelloTriangleApplication app;
+
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
